c69d8e30481efb34652011758b3cd222603136ce
shsewonitw <50475160+shsewonitw@users.noreply.github.com>
2022-09-08 11:29:57 +0900

    [Silver IV] Title: 괄호, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -5,7 +5,7 @@ T = int(sys.stdin.readline())
 for _ in range(T):
     ps = sys.stdin.readline().rstrip()
     s = []
-    isContinue = False
+    isVPS = True
     for char in ps:
         if char == "(":
             s.append(char)
@@ -13,13 +13,10 @@ for _ in range(T):
             if len(s) > 0 and s[-1] == "(":
                 s.pop()
             else:
-                print("NO")
-                isContinue = True
+                isVPS = False
                 break
-    if isContinue:
-        continue
+
     
-    if len(s) == 0:
-        print("YES")
-    else:
-        print("NO")
+    if len(s) > 0:
+        isVPS = False
+    print('YES' if isVPS else 'NO')
