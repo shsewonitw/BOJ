0e8d0105ab5885f44d97b709e8a8348138778002
shsewonitw <50475160+shsewonitw@users.noreply.github.com>
2022-10-07 12:28:17 +0900

    [Silver III] Title: 수리공 항승, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,19 @@
+import sys
+
+N,L = map(int, input().split())
+
+holes = []
+holes = map(int, input().split())
+
+result = 0
+target = 0
+for hole in sorted(holes):
+
+    if hole <= target:
+        continue
+
+    result += 1
+    target = hole + L - 1
+    
+    
+print(result)
