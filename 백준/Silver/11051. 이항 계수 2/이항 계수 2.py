42c51fd5e9f92bc9a8f1cdce861c404bf2490ea7
shsewonitw <50475160+shsewonitw@users.noreply.github.com>
2024-03-31 21:11:47 +0900

    [Silver II] Title: 이항 계수 2, Time: 304 ms, Memory: 53656 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -2,20 +2,34 @@ import sys
 input = sys.stdin.readline
 
 MOD = 10007;
-sys.setrecursionlimit(10 ** 7)
 
+# 재귀방식(Top-Down)
+# sys.setrecursionlimit(10 ** 7)
+
+# cache = [[0] * 1001 for _ in range(1001)]
+# N , K = map(int, input().split())
+
+# def bino(n, k):
+#     if cache[n][k]:
+#         return cache[n][k]
+    
+#     if k == 0 or k == n:
+#         cache[n][k] = 1
+#     else:
+#         cache[n][k] = bino(n-1, k-1) + bino(n-1, k)
+#         cache[n][k] %= MOD
+#     return cache[n][k]
+    
+# print(bino(N, K))
+
+# 반복문방식(Bottom-Up)
 cache = [[0] * 1001 for _ in range(1001)]
 N , K = map(int, input().split())
 
-def bino(n, k):
-    if cache[n][k]:
-        return cache[n][k]
-    
-    if k == 0 or k == n:
-        cache[n][k] = 1
-    else:
-        cache[n][k] = bino(n-1, k-1) + bino(n-1, k)
-        cache[n][k] %= MOD
-    return cache[n][k]
-    
-print(bino(N, K))
+for i in range(1001):
+    cache[i][0] = cache[i][i] = 1
+    for j in range(1, i):
+        cache[i][j] = cache[i-1][j-1] + cache[i-1][j]
+        cache[i][j] %= MOD
+
+print(cache[N][K])
