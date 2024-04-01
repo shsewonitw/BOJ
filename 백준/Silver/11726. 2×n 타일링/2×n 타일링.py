a72a8fc6d079131d136d209887d5446a30cbab5c
shsewonitw <50475160+shsewonitw@users.noreply.github.com>
2024-04-01 23:43:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -8,18 +8,30 @@ n = int(input())
 
 dp = [0] * 1001
 
-def getCount(idx):
-    if dp[idx] != 0:
-        return dp[idx]
+# Top-Down
+# def getCount(idx):
+#     if dp[idx] != 0:
+#         return dp[idx]
     
-    if idx == 1:
-        dp[idx] = 1
-        return dp[idx]
-    elif idx == 2:
-        dp[idx] = 2
-        return dp[idx]
+#     if idx == 1:
+#         dp[idx] = 1
+#         return dp[idx]
+#     elif idx == 2:
+#         dp[idx] = 2
+#         return dp[idx]
     
-    dp[idx] = (getCount(idx-1) + getCount(idx-2)) % MOD
-    return dp[idx]
+#     dp[idx] = (getCount(idx-1) + getCount(idx-2)) % MOD
+#     return dp[idx]
 
-print(getCount(n))
+# print(getCount(n))
+
+# Bottom-Up
+
+for i in range(1, n+1):
+    if i == 1:
+        dp[i] = 1
+    elif i == 2:
+        dp[i] = 2
+    else:
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD
+print(dp[n])
